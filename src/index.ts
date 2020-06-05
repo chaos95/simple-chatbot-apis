5cd2b078153be63686465c2ad222cd25066b3e18
Morgan Larosa <chaos95@gmail.com>
2020-06-05 16:45:50 +1000

    Add node-fetch and build API get

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,20 @@
-var hw: string = "Hello World!";
-console.log(hw);
+import fetch from 'node-fetch';
+
+export const handler = async (event: any = {}): Promise<any> => {
+    console.log(JSON.stringify(event, null, 2));
+
+    var term: string = event.params.querystring.term;
+    console.log(`Fetching definition for ${term}`);
+
+    const API_BASE: string = 'https://api.urbandictionary.com/v0/define?term=';
+    var apiUrl: string = `${API_BASE}${term}`;
+
+    var apiResponse = await fetch(apiUrl);
+
+    var body = await apiResponse.json();
+
+    var definition: string = body.list[0].definition;
+
+    const response: string = `Definition of ${term}: ${definition}`;
+    return response;
+};
